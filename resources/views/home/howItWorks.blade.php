9927dabda2a1ac32048d7fe16aad9259cfd85e86
Anindita Roy Burman <Anindita Roy Burman>
2017-02-10 10:59:14 -0600

    updated how it works page

--- resources/views/home/howItWorks.blade.php
+++ resources/views/home/howItWorks.blade.php
@@ -1,4 +1,51 @@
 @extends('layouts.app')
 @section('content')
-    This is the How It Works page
+    <div class="container">
+        <h1 style="padding-left: 25px">How it Works? </h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-block">
+                        <h2 class="card-title">How would CapSphere <br>
+                                                match my investment needs?</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-block">
+                        <p class="card-text"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend est sapien, nec sagittis augue iaculis sit amet. Nulla ante nibh, pellentesque dapibus turpis vitae, dictum imperdiet turpis. Morbi egestas vulputate tortor. Fusce pharetra sem eu nibh venenatis, sed facilisis velit pulvinar. Vestibulum faucibus turpis magna, eu condimentum purus porta finibus. Vivamus ex dui, viverra eget arcu vel, facilisis interdum ipsum. Donec a nulla finibus, pretium eros vel, dapibus nulla. Etiam eu enim ac ex eleifend hendrerit. Cras consectetur, libero eget posuere imperdiet, diam massa blandit tellus, nec condimentum nisi justo in urna.
+
+                            Mauris felis lorem, posuere eu venenatis quis, porta eu lorem. Integer risus quam, porttitor ut malesuada at, mattis in nisi. Aliquam ac accumsan sapien. Praesent tristique eros eget laoreet ornare. Ut pretium leo non tincidunt suscipit. Duis a tellus ut nisl imperdiet malesuada eget vitae enim. Duis volutpat enim purus, gravida consectetur augue iaculis sit amet. Maecenas tempor lacus in elit interdum vehicula. </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <img src="{{asset('/images/revenueModel.png')}}" class="center-block" alt="How It Works Image" >
+    </div>
+    <div class="container center-block">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-block">
+                        <h1 class="card-title">Investor</h1>
+                        <p class="card-text">Donec ac neque a justo maximus porttitor. Integer fringilla, risus ut condimentum consequat, est dui facilisis nisl, nec vulputate neque lorem vel ligula. Sed tincidunt justo ac blandit mollis. Sed maximus hendrerit orci in consectetur. Duis gravida est id libero mattis auctor. Aliquam egestas libero ac ante faucibus, nec facilisis odio lacinia. Nunc egestas orci sed diam dictum suscipit. Sed dictum quam eu commodo vulputate. Nullam in scelerisque mauris. Quisque laoreet molestie dui, in feugiat tellus. Quisque nec nunc in urna varius facilisis eget nec dolor. Nunc quis sapien augue. Suspendisse cursus dictum diam vestibulum auctor. Nullam eu feugiat dolor, eget consequat nisi. Phasellus suscipit justo nulla, in tincidunt quam posuere nec. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-block">
+                        <h1 class="card-title">Borrower</h1>
+                        <p class="card-text" style="padding:5px">Donec ac neque a justo maximus porttitor. Integer fringilla, risus ut condimentum consequat, est dui facilisis nisl, nec vulputate neque lorem vel ligula. Sed tincidunt justo ac blandit mollis. Sed maximus hendrerit orci in consectetur. Duis gravida est id libero mattis auctor. Aliquam egestas libero ac ante faucibus, nec facilisis odio lacinia. Nunc egestas orci sed diam dictum suscipit. Sed dictum quam eu commodo vulputate. Nullam in scelerisque mauris. Quisque laoreet molestie dui, in feugiat tellus. Quisque nec nunc in urna varius facilisis eget nec dolor. Nunc quis sapien augue. Suspendisse cursus dictum diam vestibulum auctor. Nullam eu feugiat dolor, eget consequat nisi. Phasellus suscipit justo nulla, in tincidunt quam posuere nec. </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
 @endsection
