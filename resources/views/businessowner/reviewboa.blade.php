c961d2e4b1ffbb974a96903f50ee52bd049fdd77
gpandey17 <gpandey@unomaha.edu>
2017-03-14 21:57:58 -0500

    Updating business owner review app

--- resources/views/businessowner/reviewboa.blade.php
+++ resources/views/businessowner/reviewboa.blade.php
@@ -12,34 +12,55 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading"><h3>Review Pending Applications</h3></div>
-
+                @if (session('status1'))
+                    <div class="alert alert-success">
+                        Session
+                    </div>
+                @endif
                 <div class="panel-body">
                   <div class="container-responsive">
   <table class="table table-hover">
     <thead>
       <tr>
-        <th>Id</th>
+
         <th>Business Owner Name</th>
         <th>Details</th>
         <th>Action</th>
+        <th></th>
+        <th>Application Status</th>
+
       </tr>
     </thead>
     <tbody>
 
 @foreach ($businessownerapplication as $businessownerapplications)
+@if($businessownerapplications->bo_app_status == '')
+
 
 
     <tr>
-        <td>{{$businessownerapplications->id}}</td>
-        <td>{{$businessownerapplications->bo_first_name}} {{$businessownerapplications->bo_last_name}}</td>
-        <td><a href="{{url('bo_application',$businessownerapplications->id)}}" class="btn btn-primary">details</a> <button type="button" class="btn btn-primary btn-sm">Download</button></td>
-        <td><button type="button" class="btn btn-info btn-sm">Approve</button>&nbsp;<button type="button" class="btn btn-danger btn-sm">Reject</button></td>
-    </tr>
 
+        <td>{{$businessownerapplications->bo_first_name}} {{$businessownerapplications->bo_last_name}}</td>
+        <td><a href="{{url('bo_application',$businessownerapplications->id)}}" class="btn btn-primary btn-sm">Details</a>
+           <button type="button" class="btn btn-primary btn-sm">Download</button></td>
+        <td>
+          {!!Form::model($businessownerapplication,array('route'=>['bo_application.update',$businessownerapplications->id],'method'=>'PATCH'))!!}
+
+          {!! Form::submit('Approve', ['class' => 'btn btn-primary','id' =>'accept'])!!}
+          {!!Form::close() !!}</td>
+          <td>
+            {!!Form::model($businessownerapplication,array('route'=>['newsletter.update',$businessownerapplications->id],'method'=>'PATCH'))!!}
+          {!! Form::submit('Reject', ['class' => 'btn btn-primary','id' =>'reject'])!!}
+           {!!Form::close() !!}</td>
+           <td>
+          {{$businessownerapplications->bo_app_status == '' ? 'Pending' : $businessownerapplications->bo_app_status}}</td>
+    </tr>
+@endif
   @endforeach
 
         </tbody>
   </table>
+
 </div>
 </div>
 </div>
@@ -58,6 +79,7 @@
                       <th>Business Owner Name</th>
                       <th>Details</th>
                       <th>Action</th>
+                      <th>Application Status</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -65,6 +87,7 @@
       <tr>              <td>{{$businessownerapplications->bo_first_name}} {{$businessownerapplications->bo_last_name}}</<td>
                       <td></td>
                       <td></td>
+                      <td>{{$businessownerapplications->bo_app_status == '' ? 'Pending' : $businessownerapplications->bo_app_status}}</td>
 </tr>
 @endforeach
                       </tbody>
