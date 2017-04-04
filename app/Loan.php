29f1983bfdef50610fa4b3a1ae91cf80dd3bf70c
ravik <rsinghania@unomaha.edu>
2017-04-03 19:55:54 -0500

    Disbursements and Loan amortizations

--- app/Loan.php
+++ app/Loan.php
@@ -13,4 +13,16 @@ class Loan extends Model
     public function user() {
         return $this->belongsTo('App\BusinessApplication');
     }
+    public function disbursement() {
+        return $this->hasmany('App\Disbursements');
+    }
+    public function amortize() {
+        return $this->hasmany('App\LoanAmortization');
+    }
+    public function trustee() {
+        return $this->hasmany('App\Trustee');
+    }
+    public function disburses() {
+        return $this->hasmany('App\Disbursement');
+    }
 }
---
+++ database/migrations/2017_04_04_002035_create_loan_amortization_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoanAmortizationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loan_amortization', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('loan_id');
+            $table->bigInteger('monthly_payment');
+            $table->bigInteger('total_amount_paid');
+            $table->bigInteger('amount_remaining');
+            $table->bigInteger('interest_amount');
+            $table->date('due_date');
+            $table->string('paid_status');
+            $table->integer('month');
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+        });
+        Schema::table('loan_amortization', function (Blueprint $table) {
+            $table->foreign('loan_id')->references('id')->on('loans')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loan_amortization');
+    }
+}
